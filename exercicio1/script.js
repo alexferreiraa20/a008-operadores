ad44c2be708a4e50f0eb0732e92a49d33e26c2c4
Alex <alexferreiraa20@gmail.com>
2022-08-30 13:16:02 -0300

    Exercicios Resolvidos Alex Ferreira - Ammal A

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const numeroPar = Number(prompt("Digite um número par:"))
+
+
+console.log(numeroPar % 2)
+console.log(typeof numeroPar)
+//no código acima, o programa executa perfeitamente a divisão dos números pares por 2 e retorna o resto da divisão que é 0
+//Quando inserimos um número ímpar no console, o resto se altera para 1, pois é o que sobra da divisão
---
+++ exercicio3/script.js
@@ -0,0 +1,10 @@
+// let informativoA
+// let informativoB
+// let informativoC
+// let informativoD
+
+console.log(5 > 20 && 5 < 2)
+console.log(5 === 5 || "5")
+console.log(!20 > 50)
+console.log(!20 > 50 || !50 > 20)
+
